d08d86d34a9b29daa058bdece999b42145262312
sthedrick <sthedrick@users.noreply.github.com>
2023-12-06 20:55:39 +0000

    Include priming of Views for Chapter 5

--- Chapter 5/Test Select V_Orders.sql
+++ Chapter 5/Test Select V_Orders.sql
@@ -1,9 +1,10 @@
-CREATE OR REPLACE VIEW V_Orders AS
+
+CREATE VIEW V_Orders AS
 SELECT O.CreationDate AS OrderDate,
   CONCAT(S.FirstName, ' ', S.LastName) AS Salesperson,
   COUNT(1) AS 'Amount of Orders',
   SUM(TotalDue) AS 'Total Due'
 FROM Orders O
   join Salesperson S ON(O.SalespersonID = S.SalespersonID)
-GROUP BY concat(S.FirstName, ' ', S.LastName),O.CreationDate
-  
+GROUP BY O.CreationDate,
+  concat(S.FirstName, ' ', S.LastName);
